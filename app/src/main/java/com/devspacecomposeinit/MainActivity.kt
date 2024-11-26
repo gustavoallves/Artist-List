99b5c9f41eec2e66786e0b905e1134d83a9e6fc9
Gustavo Alves <gu.alvesslv@gmail.com>
2024-11-26 18:44:27 -0300

    • Adicionado função de click nos cards • AlertDialog exibe ao clicar nos cards

--- app/src/main/java/com/devspacecomposeinit/MainActivity.kt
+++ app/src/main/java/com/devspacecomposeinit/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.devspacecomposeinit
 
+import android.app.AlertDialog
 import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,11 +19,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,6 +49,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val leonardo = Artist(
                         name = "Leonardo da Vinci",
+                        art = "Mona Lisa",
                         lastSeenOnline = "3 minutes ago",
                         image = R.drawable.ic_leonardo_da_vinci,
                         cardImage = R.drawable.ic_mona_lisa
@@ -52,6 +57,7 @@ class MainActivity : ComponentActivity() {
 
                     val picasso = Artist(
                         name = "Pablo Picasso",
+                        art = "O beijo",
                         lastSeenOnline = "5 minutes ago",
                         image = R.drawable.ic_pablo_picasso,
                         cardImage = R.drawable.ic_beijo
@@ -59,6 +65,7 @@ class MainActivity : ComponentActivity() {
 
                     val salvador = Artist(
                         name = "Salvador Dali",
+                        art = "Persistence of Memory",
                         lastSeenOnline = " 7 minutes ago",
                         image = R.drawable.ic_salvador_dali,
                         cardImage = R.drawable.ic_persistence_of_memory
@@ -66,6 +73,7 @@ class MainActivity : ComponentActivity() {
 
                     val vanGogh = Artist(
                         name = "Vincent Van Gogh",
+                        art = "Starry Night",
                         lastSeenOnline = "10 minutes ago",
                         image = R.drawable.ic_vincent_van_gogh,
                         cardImage = R.drawable.ic_starry_night
@@ -73,9 +81,20 @@ class MainActivity : ComponentActivity() {
 
                     val artists = listOf(salvador, picasso, vanGogh, leonardo)
 
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                    builder
+                        .setMessage("")
+                    val dialog: AlertDialog = builder.create()
+
                     LazyColumn {
                         items(artists) { artist ->
-                            ArtistCard(artist = artist)
+                            ArtistCard(
+                            artist = artist,
+                            onClick = {
+                                dialog.setMessage("' " + artist.art + " '" + " de " + artist.name + ".")
+                                dialog.show()
+                            }
+                            )
                         }
                     }
                 }
@@ -85,10 +104,14 @@ class MainActivity : ComponentActivity() {
 
 
     @Composable
-    fun ArtistCard(artist: Artist) {
+    fun ArtistCard(
+        artist: Artist,
+        onClick: () -> Unit
+    ) {
         Column(
             modifier = Modifier
                 .padding(8.dp)
+                .clickable ( onClick = onClick )
         ) {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Image(
@@ -129,6 +152,7 @@ class MainActivity : ComponentActivity() {
 
     data class Artist(
         val name: String,
+        val art: String,
         val lastSeenOnline: String,
         @DrawableRes val image: Int,
         @DrawableRes val cardImage: Int
@@ -139,12 +163,27 @@ class MainActivity : ComponentActivity() {
     fun ArtistCardPreview() {
         ComposeInitTheme {
             val artist = Artist(
-                name = "Alfred Sisley",
+                name = "Leonardo da Vinci",
+                art = "Mona Lisa",
                 lastSeenOnline = "3 minutes ago",
                 image = R.drawable.ic_leonardo_da_vinci,
                 cardImage = R.drawable.ic_mona_lisa
             )
-            ArtistCard(artist)
+
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder
+                .setMessage("")
+
+            val dialog: AlertDialog = builder.create()
+
+        ArtistCard(
+            artist = artist,
+            onClick = {
+                dialog.setMessage("É uma arte de " + artist.name)
+                dialog.show()
+            }
+        )
+
         }
     }
 }
